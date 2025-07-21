d7ec509a8cda2357162b9477dffb13ff5c593373
İbrahim Erbaş <ibrahim.erbas@woc.com.tr>
2025-07-21 14:05:21 +0300

    ...

--- ChatApp.Data/ChatAppModel.Designer.cs
+++ ChatApp.Data/ChatAppModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ibrahimerbas\Documents\Projects\WebProjects\ChatApp.Web\ChatApp.Data\ChatAppModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ibrahimerbas\source\repos\ChatApp\ChatApp.Data\ChatAppModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
